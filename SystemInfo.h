45d878c8910ecd5811be9779792fc1a343809aee
Diveturtle93 <Hafemeister1@hotmail.de>
2022-08-26 07:58:26 +0200

    Behebe Fehler in Headerfile

--- SystemInfo.h
+++ SystemInfo.h
@@ -47,7 +47,7 @@ typedef enum {
 
 // Funktionen definieren
 //----------------------------------------------------------------------
-void collectSystemInfo(void));												// Sammelt Systeminformationen, Schreibt diese auf Uart
+void collectSystemInfo(void);												// Sammelt Systeminformationen, Schreibt diese auf Uart
 reset_reason readResetSource(void);											// Wertet Systemreset aus
 void printResetSource(reset_reason reset_flags);							// Schreibt Systemreset auf Uart
 void collectHardwareInfo(void);												// Sammle Hardware Informationen
